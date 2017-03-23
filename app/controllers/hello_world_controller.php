ea136348ac41fcf010e67c06f2392b7323c90972
Juho Niittylä <juho.niittyla@helsinki.fi>
2017-03-23 13:09:00 +0200

    html-sivujen suunnitelmia

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,27 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('suunnitelmat/index.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function bethistory() {
+        View::make('suunnitelmat/bethistory.html');
+    }
+
+    public static function bet() {
+        View::make('suunnitelmat/bet.html');
+    }
+    
+    public static function balance() {
+        View::make('suunnitelmat/balance.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/bethistory', function() {
+    HelloWorldController::bethistory();
+  });
+  
+  $routes->get('/bet', function() {
+    HelloWorldController::bet();
+  });
+  
+  $routes->get('/balance', function() {
+    HelloWorldController::balance();
+  });
