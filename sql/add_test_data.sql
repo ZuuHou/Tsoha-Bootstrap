c0f7fab1dbf96b88be6a0fffefde2aef71415045
Juho Niittylä <juho.niittyla@helsinki.fi>
2017-03-24 14:20:32 +0200

    db test cases added

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,14 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+-- Gbuser-taulun testidata
+INSERT INTO Gbuser (name, password, balance) VALUES ('Hiistoni', 'H1', '100.00'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Gbuser (name, password, balance) VALUES ('Pesken', 'P1', '1000.00');
+-- Ticket taulun testidata
+INSERT INTO Ticket (amount, currentstate, added) VALUES ('10.00', NULL, '2016-11-11');
+INSERT INTO Ticket (amount, currentstate, added) VALUES ('20.00', TRUE, '2017-02-08');
+
+-- Bet taulun testidata
+INSERT INTO Bet (sport, result, odds) VALUES ('NHL', '', '2.00');
+INSERT INTO Bet (sport, result, odds) VALUES ('e-sports', 'NaVi', '5.50');
+
+-- Betticket taulun testidata
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,25 +4,25 @@ CREATE TABLE Gbuser(
   id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
   name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
   password varchar(50) NOT NULL,
-  balance decimal(10,3)
+  balance decimal(10,2)
 );
 
 CREATE TABLE Ticket(
   id SERIAL PRIMARY KEY,
-  gbuser_id INTEGER REFERENCES Gbuser(id), -- Viiteavain Player-tauluun
-  amount decimal(10,3) NOT NULL,
+  gbuser_id INTEGER REFERENCES Gbuser(id), -- Viiteavain Gbuser-tauluun
+  amount decimal(10,2) NOT NULL,
   currentstate boolean DEFAULT NULL,
   added DATE
 );
 
 CREATE TABLE Bet(
   id SERIAL PRIMARY KEY,
-  sport varchar(50) NOT NULL,
+  sport varchar(50),
   result varchar(50),
-  odds decimal(10,3)
+  odds decimal(10,2)
 );
 
 CREATE TABLE Betticket(
-  bet_id INTEGER REFERENCES bet(id), -- Viiteavain Player-tauluun
-  ticket_id INTEGER REFERENCES ticket(id)
+  bet_id INTEGER REFERENCES bet(id), -- Viiteavain Bet-tauluun
+  ticket_id INTEGER REFERENCES ticket(id) -- Viiteavain Ticket-tauluun
 );
